eba6f28d30e37918800b10fa1565e6fc4282c15a
serrev <64636780+serrev@users.noreply.github.com>
2022-03-25 14:05:36 +0100

    Delete Script_TDS directory

--- Script_TDS/f_FP.m
+++
@@ -1,3 +0,0 @@
-function [lv_FP] = f_FP(lv_nj,lv_nk,lv_nl,lv_f,lv_c,lv_dk)
-
-lv_FP = 1./(1+f_reflect(lv_nj,lv_nk).*f_reflect(lv_nk,lv_nl).*(f_propagation(lv_nk,lv_f,lv_c,lv_dk)).^2);
--- Script_TDS/f_propagation.m
+++
@@ -1,3 +0,0 @@
-function [lv_p] = f_propagation(lv_nj,lv_f,lv_c,lv_d)
-
-lv_p = exp(-1i*(lv_f*2*pi/lv_c)*lv_d.*lv_nj);
