a6e613bd7951d3524197fc8c8289b0160c6864ce
serrev <64636780+serrev@users.noreply.github.com>
2023-05-23 10:32:34 +0200

    Update f_getcomplexfield.m

--- Script_TDS/f_getcomplexfield.m
+++ Script_TDS/f_getcomplexfield.m
@@ -12,9 +12,9 @@ function [m_field] = f_getcomplexfield(lv_n0,lv_n1,lv_n2,lv_n3,lv_n4,c_c,lv_d0,l
 %           f_trans(lv_n2,lv_n3).*f_propagation(lv_n3,lv_freq,c_c,lv_d3).*f_trans(lv_n3,lv_n4).*f_propagation(lv_n4,lv_freq,c_c,lv_d4);
 
 % % Without FP AMA
-% m_field = f_trans(lv_n1,lv_n2).*f_propagation(lv_n2,lv_freq,c_c,lv_d2).*...
-%           f_trans(lv_n2,lv_n3);
+m_field = f_trans(lv_n1,lv_n2).*f_propagation(lv_n2,lv_freq,c_c,lv_d2).*...
+          f_trans(lv_n2,lv_n3);
 
 % With FP AMA
-m_field = f_trans(lv_n1,lv_n2).*f_propagation(lv_n2,lv_freq,c_c,lv_d2).*...
-          f_trans(lv_n2,lv_n3).*f_FP(lv_n1,lv_n2,lv_n3,lv_freq,c_c,lv_d2);
+% m_field = f_trans(lv_n1,lv_n2).*f_propagation(lv_n2,lv_freq,c_c,lv_d2).*...
+%           f_trans(lv_n2,lv_n3).*f_FP(lv_n1,lv_n2,lv_n3,lv_freq,c_c,lv_d2);
