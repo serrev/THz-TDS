568ea297120a850ac1f4529148e6d8dddf29841b
serrev <64636780+serrev@users.noreply.github.com>
2023-05-24 15:43:08 +0200

    Update S_TDS_ASMSA.m

--- Script_TDS/S_TDS_ASMSA.m
+++ Script_TDS/S_TDS_ASMSA.m
@@ -17,6 +17,7 @@ clc, clf, close all, clear all
 f_namedata = 'Sample';
 % Constants
 c_c = 299792458; % Light velocity
+c_e0 = 8.8541878128e-12;
 % Variables
 lv_f = 0;
 v_dsu = 1.06E-3; % Substrate thickness
@@ -29,8 +30,8 @@ v_fmin = 0.3e12;             % minimum value of interesting frequency domain
 v_fmax = 1.8e12;             % maximum value of interesting frequency domain
 %%
 % Files
-v_filenameRef = 'TDS_ReferenceSandwich_143.5-169-0.05ps_6avg_100ms-50mV20210721.dat'; % Insert file name for reference
-v_filenameSam = 'TDS_Pr'; % Insert file name for sample
+v_filenameRef = 'TDS_Air_20210427.dat'; % Insert file name for reference
+v_filenameSam = 'TDS_pType-0.3-0.5Ohmcm_20210427.dat'; % Insert file name for sample
 v_directory = strcat(pwd,'\Data_files\');
 f_FileEref = strcat(v_directory,v_filenameRef);
 f_FileEmod = strcat(v_directory,v_filenameSam);
@@ -86,7 +87,8 @@ legend('Ref','Mod','Delta')
 
 % Retrieve initial refrac
 a_nreal = 1+(abs(abs(a_ModPhase)-abs(a_RefPhase)))./(2*pi*a_freq*v_d/c_c);
-a_nimag = -(c_c./(v_d*4*pi.*a_freq)).*log(a_ModAmpl./a_RefAmpl);
+a_R = ((1-a_nreal)/(1+a_nreal)).^2;
+a_nimag = -(c_c./(v_d*4*pi.*a_freq)).*log((a_ModAmpl*(1-a_R))./a_RefAmpl);	
 a_z0 = a_nreal + 1i*a_nimag;
 
 lv_f = lv_f + 1;
@@ -98,12 +100,12 @@ legend('Real','Imag')
 %% Getting matrix of solutions
 % Refract matrix (triangular matrix, because yes... In principle the resolution is higher with less datapòints that one rectangular matrix)
 % z = x + 1i*y; x:[xb,xe]; y:[yb,ye]; r: step
-xb = 1;
-xe = 12;
-yb = 0.1;
-ye = 3;
+v_xb = 1;
+v_xe = 12;
+v_yb = 0.1;
+v_ye = 3;
 
-r = 0.005;
+v_r = 0.005;
 
 v_X=v_xe-v_xb;
 v_Y=v_ye-v_yb;
